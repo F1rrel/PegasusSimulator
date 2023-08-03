ef463031d72a7f7405abe3f237b040a8c12f5c31
Filip Stec <filip.stec@airvolute.com>
2023-08-03 11:48:50 +0200

    ROS2Odometry

--- extensions/pegasus.simulator/pegasus/simulator/logic/graphs/__init__.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/graphs/__init__.py
@@ -5,3 +5,4 @@
 from .graph import Graph
 from .ros2_camera import ROS2Camera
 from .ros2_tf import ROS2Tf
+from .ros2_odometry import ROS2Odometry
--- extensions/pegasus.simulator/pegasus/simulator/parser/graphs_parser.py
+++ extensions/pegasus.simulator/pegasus/simulator/parser/graphs_parser.py
@@ -5,7 +5,7 @@
 
 # Graphs that can be used with the vehicles
 from pegasus.simulator.parser import Parser
-from pegasus.simulator.logic.graphs import ROS2Camera, ROS2Tf
+from pegasus.simulator.logic.graphs import ROS2Camera, ROS2Tf, ROS2Odometry
 
 
 class GraphParser(Parser):
@@ -14,7 +14,8 @@ class GraphParser(Parser):
         # Dictionary of available graphs to instantiate
         self.graphs = {
             "ROS2 Camera": ROS2Camera,
-            "ROS2 Tf": ROS2Tf
+            "ROS2 Tf": ROS2Tf,
+            "ROS2 Odometry": ROS2Odometry
         }
 
     def parse(self, data_type: str, data_dict):
